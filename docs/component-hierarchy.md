e1d36c3b9fd590f4d0560cd11eae993c7d47b1f8
Tara Mehryar <tara.mehryar@gmail.com>
2023-09-05 05:51:16 -0500

    docs

---
+++ docs/component-hierarchy.md
@@ -0,0 +1,9 @@
+#Component hierarchy
+
+-App.jsx(Homepage -- All Recipes)
+    -RecipeList(Individual Card)
+        -Editable Title
+        -Editable Ingredients
+        -Editable Steps
+    -AddRecipes (ability to add more recipes on homepage)
+    
---
+++ docs/data-mockup.js
@@ -0,0 +1,9 @@
+[
+    {
+        id: id++,
+        title: "Scrambled Eggs",
+        ingredients: "2 Eggs, 1 tbsp butter, 1/4 cup milk",
+        steps: "First crack eggs into container. Whisk eggs with milk. Add butter to pan, let pan heat up and pour milk and egg mixture. Stir constantly"
+
+    }
+]
